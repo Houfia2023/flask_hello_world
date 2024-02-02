0de9ec586c34c67eee134cd23de1676602e8d90b
Lotfi_MHAMDI <150040662+Houfia2023@users.noreply.github.com>
2024-02-02 15:00:35 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+  
 @app.route("/fr/")
 def monfr():
     return "<h2>Bonjour tout le monde !</h2>"
