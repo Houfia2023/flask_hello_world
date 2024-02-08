0745b4757ba9147b49e6707bba237a6c5a3df46b
Lotfi_MHAMDI <150040662+Houfia2023@users.noreply.github.com>
2024-02-08 09:54:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,3 +31,13 @@ def mongraphique():
 if __name__ == "__main__":
   app.run(debug=True)
 
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
