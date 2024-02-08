cabc581d1780d7d5e23b3e6b6a263ab9c1805d04
Lotfi_MHAMDI <150040662+Houfia2023@users.noreply.github.com>
2024-02-08 12:12:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -50,16 +50,6 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
     
-@app.route('/fiche_client/<string:Nom>')
-def Readfiche(Nom):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE Nom Like ?', (Nom,))
-    data = cursor.fetchall()
-    conn.close()
-    
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
 
 if __name__ == "__main__":
   app.run(debug=True)
